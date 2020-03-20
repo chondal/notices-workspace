4e59b68cb4a1da5c9f0fa9db600dc2651e7a5964
Jonathan <jg.pradi@gmail.com>
2020-03-20 14:12:14 -0300

    fix views publish

--- src/NoticesWorkspaceServiceProvider.php
+++ src/NoticesWorkspaceServiceProvider.php
@@ -1,40 +1,38 @@
 <?php
 namespace Chondal\NoticesWorkspace;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class NoticesWorkspaceServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views/', 'NoticesWorkspace');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'NoticesWorkspace');
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         $this->publishes([
-            __DIR__.'/../database/migrations' => database_path('migrations')
+            __DIR__ . '/../database/migrations' => database_path('migrations'),
         ], 'notices-workspace-migrations');
 
         $this->publishes([
-            __DIR__.'/../resources/views' => resource_path('views/vendor/notices-workspace')
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/NoticesWorkspace'),
         ], 'notices-workspace-views');
 
         $this->publishes([
-            __DIR__.'/../config/notices-workspace.php' => base_path('config/notices-workspace.php')
+            __DIR__ . '/../config/notices-workspace.php' => base_path('config/notices-workspace.php'),
         ], 'notices-workspace-config');
-
 
     }
 
     public function register()
     {
-        $this->app->bind('notices-workspace', function(){
+        $this->app->bind('notices-workspace', function () {
             return new Notice;
         });
 
-        $this->mergeConfigFrom(__DIR__.'/../config/notices-workspace.php','notices-workspace');
+        $this->mergeConfigFrom(__DIR__ . '/../config/notices-workspace.php', 'notices-workspace');
     }
 }
